596370468ebf1d23bc21d5b72487e852bcba42aa
ZhangYuzhe <33855178+Zhang-YZ@users.noreply.github.com>
2026-08-20 22:56:48 +0800

    Publish wedding site from docs

---
+++ docs/script.js
@@ -0,0 +1,15 @@
+const dialog = document.querySelector("#lightbox");
+const preview = dialog.querySelector("img");
+
+document.querySelectorAll(".photo").forEach((photo) => {
+  photo.addEventListener("click", () => {
+    preview.src = photo.dataset.full;
+    preview.alt = photo.querySelector("img").alt;
+    dialog.showModal();
+  });
+});
+
+dialog.querySelector("button").addEventListener("click", () => dialog.close());
+dialog.addEventListener("click", (event) => {
+  if (event.target === dialog) dialog.close();
+});
